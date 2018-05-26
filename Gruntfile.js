b17f6d5c2829c75a16eb090a6c924150168fc9dc
Jasper <jasperthebest@users.noreply.github.com>
2018-05-26 22:32:11 +0530

    subpages responsive

--- Gruntfile.js
+++ Gruntfile.js
@@ -45,7 +45,8 @@ module.exports = function(grunt) {
     connect: {
       server: {
         options: {
-          hostname: '192.168.1.6',
+          //hostname: '192.168.1.6',
+          hostname: '192.168.0.101',
           port: 8000,
           base: '.',
           livereload: true
